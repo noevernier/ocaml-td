ac1cf5d4914a3d7b476850d4a2ad2b7004cdec95
Noé Vernier <vernier.noe@gmail.com>
2021-09-21 20:07:47 +0200

    infos abr function

--- td2.ml
+++ td2.ml
@@ -1,3 +1,4 @@
+(*--------------------EXERCICE 1--------------------*)
 type 'a arbre = Vide | N of ('a arbre)*('a)*('a arbre);;
 
 let rec imprime_infixe (a : 'a arbre) =
@@ -17,7 +18,7 @@ imprime_prefixe (N(N(Vide, 2, Vide), 1, N(N(Vide, 6, Vide), 4, Vide)));;
 let rec imprime_postfixe (a : 'a arbre) =
     match a with
     |Vide -> ()
-    |N(g, e, d) -> (imprime_infixe g); (imprime_infixe d); (print_int e);;;
+    |N(g, e, d) -> (imprime_infixe g); (imprime_infixe d); (print_int e);;
 
 imprime_postfixe (N(N(Vide, 2, Vide), 1, N(N(Vide, 6, Vide), 4, Vide)));;
 
@@ -26,6 +27,7 @@ let rec profondeur (a : 'a arbre) =
     |Vide -> 0
     |N(g, e, d) -> 1 + (max (profondeur g) (profondeur d));;
 
+(*--------------------EXERCICE 2--------------------*)
 let rec liste_noeuds (a : 'a arbre) (n : int) =
     match (n, a) with
     |(0, a) ->  let rec get_node_list (a : 'a arbre) = 
@@ -39,7 +41,7 @@ let rec liste_noeuds (a : 'a arbre) (n : int) =
 liste_noeuds (N(N(Vide, 2, Vide), 1, N(N(Vide, 6, Vide), 4, Vide))) 0;;
 
 let largeur (a : 'a arbre) = liste_noeuds a 0;;
-
+(*--------------------EXERCICE 3--------------------*)
 type abr = Vide | N of abr*(int)*abr;;
 
 
@@ -86,11 +88,21 @@ let rec creer_abr (l : int list) =
     |[] -> Vide
     |t::q -> insere_feuille (creer_abr q) t;;
 
-let insere_racine (a : abr) (x : int) = creer_abr(x::(list_infixe a));;
+let insere_racine_bis (a : abr) (x : int) = creer_abr(x::(list_infixe a));;
+
+let rec infos (a : abr) = 
+    match a with
+    |Vide -> failwith "Arbre vide"
+    |N(Vide, x, Vide) -> [|x;x;1|]
+    |N(g,x,Vide) -> let i = infos g in  if(x >= i.(1)) then [|i.(0); x;1|] else [|i.(0); x; 1|] 
+    |N(g,x,d) -> let ig = infos g and id = infos d in
+                if((ig.(2)==1) && (id.(2)==1) && (x >= ig.(1)) && (x <= id.(0))) then [|ig.(0); id.(1); 1|] 
+                else [|ig.(0); id.(1); 0|];;
+
+infos (creer_abr [1;4;3;5;8;23;3]);;
 
 
-creer_abr [1;4;3;5;8;23;3];;
-
+(*--------------------EXERCICE 4--------------------*)
 type operation = Plus | Mult | Div | Minus;;
 
 type abr_expr = Zero | F of int | N of operation * abr_expr * abr_expr;;
@@ -125,3 +137,6 @@ let rec imprime_postfixe (a : abr_expr) =
     |N(Minus, g, d) -> (print_string "("); (imprime_infixe g);  (imprime_infixe d);(print_string "-");(print_string ")");;
 
 imprime_postfixe (N(Minus, N(Mult,N(Plus, F(4), F(3)), N(Div, N(Minus, Zero, F(12)), F(2))), F(5)));;
+
+
+let rec abre_of_infixe e =
